5efb5446534464116553905846812bef8bdfac61
Matt Savage <141598521+mattsavage01@users.noreply.github.com>
2025-03-09 20:52:01 -0500

    Fixed to save rsa key for future decryption

--- rsa/powershell/cipher_rsa_enter_encrypted.ps1
+++ rsa/powershell/cipher_rsa_enter_encrypted.ps1
@@ -1,38 +1,31 @@
-if (!(Test-Path -Path 'rsa.pub'))
+if (Test-Path -Path 'rsa.key')
 {
-    # Generate an RSA key pair
-    $rsa = New-Object System.Security.Cryptography.RSACryptoServiceProvider
-    $publicKey = $rsa.ToXmlString($false) # false for public key only
-    $privateKey = $rsa.ToXmlString($true)  # true for both public and private keys
+    $privateKey = Get-Content 'rsa.key'
 
-    $publicKey | Out-File "rsa.pub"
-    $privateKey | Out-File "rsa.key"
+    # Data to encrypt
+    $data = $($args[0])
+    if ($null -eq $data)
+    {
+        Write-Host "Already Encrypt Text?"
+        $data = Read-Host
+    }
+    $encoding = [System.Text.Encoding]::UTF8
+
+    # Decrypt the data using the private key
+    $rsaDecrypt = New-Object System.Security.Cryptography.RSACryptoServiceProvider
+    $rsaDecrypt.FromXmlString($privateKey)
+    $encryptedBytes = [Convert]::FromBase64String($data)
+    $decryptedBytes = $rsaDecrypt.Decrypt($encryptedBytes, $false) # false for PKCS1 padding
+    $decryptedText = $encoding.GetString($decryptedBytes)
+
+    # Output
+    Write-Host "Original Text: $data"
+    Write-Host "Decrypted Text: $decryptedText"
+
+    # Clean up resources
+    $rsa.Dispose()
+    $rsaDecrypt.Dispose()
 }
 else {
-    #$publicKey = Get-Content 'rsa.pub'
-    $privateKey = Get-Content 'rsa.key'
+    Write-Host "No RSA key exsits. Will NOT be able to decrypt text!!!"
 }
-
-# Data to encrypt
-$data = $($args[0])
-if ($null -eq $data)
-{
-    Write-Host "Already Encrypt Text?"
-    $data = Read-Host
-}
-$encoding = [System.Text.Encoding]::UTF8
-
-# Decrypt the data using the private key
-$rsaDecrypt = New-Object System.Security.Cryptography.RSACryptoServiceProvider
-$rsaDecrypt.FromXmlString($privateKey)
-$encryptedBytes = [Convert]::FromBase64String($data)
-$decryptedBytes = $rsaDecrypt.Decrypt($encryptedBytes, $false) # false for PKCS1 padding
-$decryptedText = $encoding.GetString($decryptedBytes)
-
-# Output
-Write-Host "Original Text: $data"
-Write-Host "Decrypted Text: $decryptedText"
-
-# Clean up resources
-$rsa.Dispose()
-$rsaDecrypt.Dispose()
